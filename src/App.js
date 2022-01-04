3a29ac625efe7ba5062b5170fd18bd36818bae9f
haditorabi <haditorabi1984@gmail.com>
2022-01-04 13:04:55 -0500

    Added sticky Navbar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
   return (
     <>
       <div className="bg-main-50 min-h-screen pb-10">
-        <div className="bg-actionSecondary-50 text-basicSecondary-500 shadow-sm lg:overflow-y-visible">
+        <div className="bg-actionSecondary-50 text-basicSecondary-500 shadow-sm lg:overflow-y-visible sticky w-full z-50">
           <Navbar />
         </div>
         {/* main container */}
-        <div className="max-w-9xl  mx-0 sm:px-0 lg:px-0 w-full grid grid-cols-12 gap-5 overflow-hidden pb-5 h-screen">
+        <div className="max-w-9xl mx-0 sm:px-0 lg:px-0 w-full grid grid-cols-12 gap-5 overflow-y-auto pb-5 h-screen">
           <LeftSidebar />
           <MainContent />
         </div>
